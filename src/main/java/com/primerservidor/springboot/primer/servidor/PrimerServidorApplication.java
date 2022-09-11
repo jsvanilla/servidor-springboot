db4b1f41909996d309a2a5f2b930ea44aa5391e7
jsvanilla <36719905+jsvanilla@users.noreply.github.com>
2022-09-11 00:41:51 -0500

    first commit

---
+++ src/main/java/com/primerservidor/springboot/primer/servidor/PrimerServidorApplication.java
@@ -0,0 +1,13 @@
+package com.primerservidor.springboot.primer.servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerServidorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primerservidor/springboot/primer/servidor/PrimerServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primerservidor.springboot.primer.servidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
